cda26bf2524c5b2a1c1fde8963ed0628eb55569f
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2022-09-23 17:04:41 -0400

    add build script and defconfig

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+GNU_VERSION=11.3.rel1
+
+export ARCH=arm64
+export CROSS_COMPILE=/opt/gcc-arm-${GNU_VERSION}/bin/aarch64-none-elf-
+
+# build kernel
+make radxa_zero_defconfig
+make -j8
+
+# package kernel
+export build_id="0"
+export lv="-${build_id}-amlogic"
+export kv=$(make kernelversion)
+export debv="${kv}${lv}"
+make bindeb-pkg -j8 LOCALVERSION=$lv KDEB_PKGVERSION=$debv
