83aa095452b9575db913226110cae56d921bdcaa
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2022-09-27 00:04:42 -0400

    add radxa overlays and zero2 support

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# exit when any command fails
+set -e
+
 export ARCH=arm64
 export CROSS_COMPILE=${ARM_GCC_PATH}/bin/aarch64-none-elf-
 
@@ -14,4 +17,4 @@ export build_id="$(date '+%Y%m%d%H%M')"
 export lv="-${build_id}-amlogic"
 export kv=$(make kernelversion)
 export debv="${kv}${lv}"
-make bindeb-pkg -j8 LOCALVERSION=$lv # KDEB_PKGVERSION=$debv
+make bindeb-pkg -j8 LOCALVERSION=$lv KDEB_PKGVERSION=$debv
